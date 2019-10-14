916a945d5b75b5e9733df7ee96aabd2aff4596b3
maxThoor <maxime.thoor@isen.yncrea.fr>
2019-10-14 15:46:13 +0200

    Test
    Changement

--- TraitementDatasPython/fonctionCSV.py
+++ TraitementDatasPython/fonctionCSV.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def extractCSV(CSVName,listOfColumns):
     df = pd.read_csv("indice-qualite-de-lair.csv", header=0, delimiter=';')[listOfColumns]
-    df.to_csv("newFileTest.csv", index=False)
+    df.to_csv("notreDataSet.csv", index=False)
 
 if __name__ == '__main__':
     myList = ["valeur","type_zone"]
