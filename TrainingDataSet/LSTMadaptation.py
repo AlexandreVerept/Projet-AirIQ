ab288d973dcbe0b2d78e227d8707dacf8b445de6
AV <alexandre.verept@isen.yncrea.fr>
2019-11-12 14:51:02 +0100

    Adaptation LSTM
    Début adaptation de la LSTM de tensorflow weather

---
+++ TrainingDataSet/LSTMadaptation.py
@@ -0,0 +1,160 @@
+#tuto inspiration : https://www.tensorflow.org/tutorials/structured_data/time_series
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+# import datas
+df = pd.read_csv("Dataset.csv", header=0, delimiter=';')
+
+# print('Dataset:\n',df.head())
+
+def create_time_steps(length):
+  time_steps = []
+  for i in range(-length, 0, 1):
+    time_steps.append(i)
+  return time_steps
+
+# Va nous servir à afficher l'historique
+
+def plot_train_history(history, title):
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  epochs = range(len(loss))
+
+  plt.figure()
+
+  plt.plot(epochs, loss, 'b', label='Training loss')
+  plt.plot(epochs, val_loss, 'r', label='Validation loss')
+  plt.title(title)
+  plt.legend()
+
+  plt.show()
+  
+# Va nous servir à afficher les prévisions
+def multi_step_plot(history, true_future, prediction):
+  plt.figure(figsize=(12, 6))
+  num_in = create_time_steps(len(history))
+  num_out = len(true_future)
+
+  plt.plot(num_in, np.array(history[:, 1]), label='History')
+  plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
+           label='True Future')
+  if prediction.any():
+    plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
+             label='Predicted Future')
+  plt.legend(loc='upper left')
+  plt.show()
+
+"""
+==========NOS DONNEES:==========
+créer la fenêtre d'apprentissage:
+Nous avons une data environ toutes les 15 minutes (entre 10 et 20) 
+soit 96 mesures par jour en moyenne
+On veut prédire dans un premier temps 24h dans le futur
+==========NOS CHOIX:==========
+On choisit arbitrairement d'utiliser 5 jours pour prédire le suivant
+on utilise donc les 5 * 96 = 480 dernières observations pour le training
+"""
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+  data = []
+  labels = []
+
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
+
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i, step)
+    data.append(dataset[indices])
+
+    if single_step:
+      labels.append(target[i+target_size])
+    else:
+      labels.append(target[i:i+target_size])
+
+  return np.array(data), np.array(labels)
+
+# désigne le nombre de données utilisées pour l'entrainement
+TRAIN_SPLIT = 12000 #sur 12662 environ lors du code
+
+features_considered = ['IQ', 'PanneauS', 'TempExt','HygroExt']
+features = df[features_considered]
+features.index = df['Date']
+
+print(features.head())
+features.plot(subplots=True)
+
+# Normaliser les datas ? On ne l'a pas fait ici
+
+dataset = features.values
+#data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+#data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+#dataset = (dataset-data_mean)/data_std
+
+"""
+======= Début du multi step model ======
+"""
+future_target = 96 * 1  #(pour 1 jour)
+past_history = 96 * 5
+STEP = 6
+
+x_train_multi, y_train_multi = multivariate_data(dataset, dataset[:, 1], 0,
+                                                 TRAIN_SPLIT, past_history,
+                                                 future_target, STEP)
+
+x_val_multi, y_val_multi = multivariate_data(dataset, dataset[:, 1],
+                                             TRAIN_SPLIT, None, past_history,
+                                             future_target, STEP)
+
+print ('Single window of past history : {}'.format(x_train_multi[0].shape))
+print ('\n Target to predict : {}'.format(y_train_multi[0].shape))
+
+# shuffle
+BATCH_SIZE = 256
+BUFFER_SIZE = 10000
+
+train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
+train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
+val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
+
+# afficher
+for x, y in train_data_multi.take(2):
+    multi_step_plot(x[0], y[0], np.array([0]))
+    
+#model
+multi_step_model = tf.keras.models.Sequential()
+multi_step_model.add(tf.keras.layers.LSTM(32,
+                                          return_sequences=True,
+                                          input_shape=x_train_multi.shape[-2:]))
+multi_step_model.add(tf.keras.layers.LSTM(16, activation='relu'))
+multi_step_model.add(tf.keras.layers.Dense(96))
+
+multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mae')
+
+for x, y in val_data_multi.take(1):
+  print (multi_step_model.predict(x).shape)
+  
+# Train 
+EPOCHS =1
+EVALUATION_INTERVAL = 100
+  
+multi_step_history = multi_step_model.fit(train_data_multi, epochs=EPOCHS,
+                                          steps_per_epoch=EVALUATION_INTERVAL,
+                                          validation_data=val_data_multi,
+                                          validation_steps=50)
+
+plot_train_history(multi_step_history, 'Multi-Step Training and validation loss')
+for x, y in val_data_multi.take(3):
+  multi_step_plot(x[0], y[0], multi_step_model.predict(x)[0])
