6f8606865c9562c4ffeae1d6a1ddadd41b86a3a3
AV <alexandre.verept@isen.yncrea.fr>
2019-12-04 11:18:36 +0100

    MAJ graph displa

--- 3_Deployable/Training/Visualisation/DisplayGraphe.py
+++ 3_Deployable/Training/Visualisation/DisplayGraphe.py
@@ -10,26 +10,51 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("datas/affichagePourGrapheTemp.csv", header=0, delimiter=';')
 
-#temps
-plt.title("Temperature")
-plt.plot (data['temperatureMeteo'],label='Meteo')
-plt.plot (data['temperatureRuche'],label='Ruche')
+#===============
+#temperature
+#===============
 
+plt.title("Temperature")
+
+plt.plot(data['temperatureRuche'],label='Ruche')
+plt.plot(data['temperatureMeteo'],label='Meteo')
+
+plt.xlabel('Days')
+plt.ylabel('°C')
+plt.legend()
 plt.show()
+
+#===============
+#temperature difference
+#===============
 
 plt.title("Temperature difference")
-plt.plot(data['temperatureRuche']-data['temperatureMeteo'], label ="difference")
+plt.plot(data['temperatureRuche']-data['temperatureMeteo'], label ="difference", color ='lightgreen')
 
+plt.xlabel('Days')
+plt.ylabel('delta °C')
 plt.show()
 
-#hum
-plt.title("Humidite")
+#===============
+#humidity
+#===============
+
+plt.title("Humidity")
 plt.plot (data['humiditeMeteo'],label='Meteo')
 plt.plot (data['humiditeRuche'],label='Ruche')
 
+plt.xlabel('Days')
+plt.ylabel('humidity %')
+plt.legend()
 plt.show()
 
-plt.title("Humidite difference")
-plt.plot(data['humiditeRuche']-data['humiditeMeteo'], label ="difference")
+#===============
+#humidity difference
+#===============
 
+plt.title("Humidity difference")
+plt.plot(data['humiditeRuche']-data['humiditeMeteo'], label ="difference", color ='lightgreen')
+
+plt.xlabel('Days')
+plt.ylabel('delta humidity %')
 plt.show()
